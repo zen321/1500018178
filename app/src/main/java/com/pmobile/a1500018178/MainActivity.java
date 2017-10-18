025cd9e01c961af76d6c84b4f2fe2769a252e684
zen321 <zenrio1@gmail.com>
2017-10-18 14:17:34 +0700

    ini pertama

---
+++ app/src/main/java/com/pmobile/a1500018178/MainActivity.java
@@ -0,0 +1,13 @@
+package com.pmobile.a1500018178;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
